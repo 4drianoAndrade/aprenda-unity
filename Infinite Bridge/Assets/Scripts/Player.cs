a1e740d4b9d8beb84dcc9bc36cabe4f31d2aa482
Adriano Silva Andrade <adrandrade1995@gmail.com>
2021-12-10 00:50:45 -0300

    IMPONDO LIMITES DE MOVIMENTO, FAZENDO USO DO MASK INTERATION

--- Infinite Bridge/Assets/Scripts/Player.cs
+++ Infinite Bridge/Assets/Scripts/Player.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D playerRb;
-    public float movementSpeed;
+    public float velocityMovement;
+
+    public float limitMaxY;
+    public float limitMinY;
+    public float limitMaxX;
+    public float limitMinX;
 
     // Start is called before the first frame update
     void Start()
     {
+        Application.targetFrameRate = 60;
+
         playerRb = GetComponent<Rigidbody2D>();
     }
 
@@ -19,6 +26,31 @@ public class Player : MonoBehaviour
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
-        playerRb.velocity = new Vector2(horizontal * movementSpeed, vertical * movementSpeed);
+        float posY = transform.position.y;
+        float posX = transform.position.x;
+
+        playerRb.velocity = new Vector2(horizontal * velocityMovement, vertical * velocityMovement);
+
+        // Verifica o limite X
+        if (transform.position.x > limitMaxX)
+        {
+            posX = limitMaxX;
+        }
+        else if (transform.position.x < limitMinX)
+        {
+            posX = limitMinX;
+        }
+
+        // Verifica o limite Y
+        if (transform.position.y > limitMaxY)
+        {
+            posY = limitMaxY;
+        }
+        else if (transform.position.y < limitMinY)
+        {
+            posY = limitMinY;
+        }
+
+        transform.position = new Vector2(posX, posY);
     }
 }
