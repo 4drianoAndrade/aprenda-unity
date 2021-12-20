8ea5b96f0bba512d2179bc8d5e185741df4e1510
Adriano Silva Andrade <adrandrade1995@gmail.com>
2021-12-20 00:18:19 -0300

    SISTEMA DE PULO

--- FastBunny Adventure/Assets/Scripts/bunnyController.cs
+++ FastBunny Adventure/Assets/Scripts/bunnyController.cs
@@ -7,8 +7,9 @@ public class bunnyController : MonoBehaviour
     [Header("Config. Player")]
 
     public float forceJump;
-
     private Rigidbody2D rbPlayer;
+    public Transform groundCheck;
+    private bool grounded;
 
     // Start is called before the first frame update
     void Start()
@@ -16,10 +17,15 @@ public class bunnyController : MonoBehaviour
         rbPlayer = GetComponent<Rigidbody2D>();
     }
 
+    void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, 0.02f);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && grounded == true)
         {
             rbPlayer.AddForce(new Vector2(0, forceJump));
         }
