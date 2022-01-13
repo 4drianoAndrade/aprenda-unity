5ffa795f19796dff9cc1fabdc65746897f41a73c
Adriano Silva Andrade <adrandrade1995@gmail.com>
2022-01-13 11:13:03 -0300

    SISTEMA DE MULTIPLOS PULOS

--- 02 FastBunny Adventure/Assets/Scripts/Character.cs
+++ 02 FastBunny Adventure/Assets/Scripts/Character.cs
@@ -17,8 +17,12 @@ public class Character : MonoBehaviour
 
     public float speed;
     public float jumpForce;
+    public int extraJumps;
+    private int exJump;
 
     public bool isLeft;
+
+    public LayerMask whatIsGround;
 
     // Start is called before the first frame update
     void Start()
@@ -26,11 +30,13 @@ public class Character : MonoBehaviour
         playerRb = GetComponent<Rigidbody2D>();
         playerAnimator = GetComponent<Animator>();
         playerSr = GetComponent<SpriteRenderer>();
+
+        exJump = extraJumps;
     }
 
     void FixedUpdate()
     {
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.02f);
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.02f, whatIsGround);
     }
 
     // Update is called once per frame
@@ -60,9 +66,19 @@ public class Character : MonoBehaviour
         speedY = playerRb.velocity.y;
         playerRb.velocity = new Vector2(horizontal * speed, speedY);
 
-        if (Input.GetButtonDown("Jump") && isGrounded == true)
+        if (isGrounded == true)
         {
-            playerRb.AddForce(new Vector2(0, jumpForce));
+            exJump = extraJumps;
+        }
+
+        if (Input.GetButtonDown("Jump") && exJump > 0)
+        {
+            jump();
+            exJump--;
+        }
+        else if (Input.GetButtonDown("Jump") && exJump == 0 && isGrounded == true)
+        {
+            jump();
         }
     }
 
@@ -71,6 +87,12 @@ public class Character : MonoBehaviour
         playerAnimator.SetInteger("speedX", speedX);
         playerAnimator.SetFloat("speedY", speedY);
         playerAnimator.SetBool("isGrounded", isGrounded);
+    }
+
+    public void jump()
+    {
+        playerRb.velocity = new Vector2(playerRb.velocity.x, 0);
+        playerRb.AddForce(new Vector2(0, jumpForce));
     }
 
     // FUNÇÃO RESPONSÁVEL POR VIRAR A DIREÇÃO QUE O PERSONAGEM ESTÁ OLHANDO
