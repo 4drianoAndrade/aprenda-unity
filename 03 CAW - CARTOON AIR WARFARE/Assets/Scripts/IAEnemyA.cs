908cce05d7b0f88f721cd27c35f790689360c163
Adriano Silva Andrade <adrandrade1995@gmail.com>
2022-03-26 21:01:38 -0300

    CAW - CARTOON AIR WARFARE - PARTE 13

--- 03 CAW - CARTOON AIR WARFARE/Assets/Scripts/IAEnemyA.cs
+++ 03 CAW - CARTOON AIR WARFARE/Assets/Scripts/IAEnemyA.cs
@@ -19,6 +19,12 @@ public class IAEnemyA : MonoBehaviour
     private float incremented;
     private float zRotation;
 
+    public GameObject prefabShot;
+    public Transform gun;
+    public float shotSpeed;
+
+    public float delayShot;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,6 +33,29 @@ public class IAEnemyA : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        curveControl();
+    }
+
+    void shoot()
+    {
+        GameObject temp = Instantiate(prefabShot, gun.position, transform.localRotation);
+        temp.GetComponent<Rigidbody2D>().velocity = transform.up * -1 * shotSpeed;
+    }
+
+    void OnBecameVisible()
+    {
+        StartCoroutine("shotControl");
+    }
+
+    IEnumerator shotControl()
+    {
+        yield return new WaitForSeconds(delayShot);
+        shoot();
+        StartCoroutine("shotControl");
+    }
+
+    void curveControl()
     {
         if (transform.position.y <= curveStartingPoint && isCurve == false && directionMovement == direction.Below)
         {
